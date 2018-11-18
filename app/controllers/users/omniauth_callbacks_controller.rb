069a10e4300fcf7a6f116a9858d9e1361c0bf18e
franklin de chanaud <f_dechanaud@hotmail.fr>
2018-11-18 19:28:44 +0100

    facebook connect done

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,17 @@
 class Users::OmniauthCallbacksController < ApplicationController
   def facebook
-    if current_user.present?
-      current_user.apply_omniauth(request.env["omniauth.auth"])
-      redirect_to edit_user_registration_path, notice: "Facebook Account Linked!"
+    # raise
+    user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if user.persisted?
+      sign_in_and_redirect user, event: :authentication
     else
-      @user = User.from_omniauth(request.env["omniauth.auth"])
-      sign_in_and_redirect @user
+      session['devise.facebook_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
     end
+  end
+
+  def failure
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,26 @@
 class User < ApplicationRecord
   has_many :projects, dependent: :destroy
   has_many :comments, dependent: :destroy
-  devise :omniauthable
+  devise :omniauthable, omniauth_providers: [:facebook]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook]
+         :recoverable, :rememberable, :validatable
+
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.password = Devise.friendly_token[0,20]
+    user_params = auth.slice(:provider, :uid)
+    user_params.merge! auth.info.slice(:email, :first_name, :last_name)
+    user_params = user_params.to_h
+
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+    user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
+    if user
+      user.update(user_params)
+    else
+      user = User.new(user_params)
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.save
     end
-  end
-
-  def apply_omniauth(auth)
-    update_attributes(
-      provider: auth.provider,
-      uid: auth.uid
-    )
-  end
-
-  def has_facebook_linked?
-    self.provider.present? && self.uid.present?
+    return user
   end
 end
