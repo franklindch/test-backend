883e2dd64b0a324ccaadf545440feda00875d49f
franklin de chanaud <f_dechanaud@hotmail.fr>
2018-11-19 09:24:00 +0100

    main controller actions done

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,38 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :retrieve_comment, only: [:destroy]
+  before_action :retrieve_task, only: [:new, :create]
+
+  def new
+    @comment = Comment.new
+    respond_to :html, :js
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.task = @task
+    @comment.user = current_user
+    @comment.save ? (redirect_to projects_path) : (render :new)
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to projects_path
+  end
+
+  private
+
+  def retrieve_comment
+    @comment ||= Comment.find(params[:id])
+  end
+
+  def retrieve_task
+    @task ||= Task.find(params[:task_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(
+      :description, :file, :task_id
+    )
+  end
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  has_many :users
+  has_many :comments
   belongs_to :project
 end
