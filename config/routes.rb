d6c7d30ace88a30b06c19cfc43b2c18f36527c5d
franklin de chanaud <f_dechanaud@hotmail.fr>
2018-11-19 08:48:09 +0100

    main actions done

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     root 'devise/sessions#new'
   end
 
-  resources :projects, except: [:show]
+  resources :projects, except: [:show] do
+    resources :tasks, only: [:new, :create, :edit, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
