a82f0a93c5bd0371dc6d580fbc5a970c14467ecd
franklin de chanaud <f_dechanaud@hotmail.fr>
2018-11-19 08:39:28 +0100

    create, update, delete project actions done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   authenticated :user do
-    root 'pages#home', as: 'authenticated_root'
+    root 'projects#index', as: 'authenticated_root'
   end
   devise_scope :user do
     root 'devise/sessions#new'
   end
+
+  resources :projects, except: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Project.create!(
+  name: 'Backoffice Forest'
+  user_id: User.first
+)
